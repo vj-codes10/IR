4128d7167ef03c243a749060494f54155b87850e
vj-codes10 <101552621+vj-codes10@users.noreply.github.com>
2022-03-14 18:57:46 +0530

    IR 3rd

---
+++ pract3rd.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 14 18:15:15 2022
+
+@author: Vijay shelke
+"""
+"""
+ Implement Dynamic programming algorithm for computing the edit distance between strings s1 and s2.
+"""
+import numpy as np 
+def levenshelin(s1,s2):
+    size_x=len(s1)+1
+    size_y=len(s2)+1
+    matrix=np.zeros((size_x,size_y))
+    for x in range(size_x):
+        matrix[x,0]=x
+        for y in range(size_y):
+            matrix[0,y]=y
+            for x in range(1,size_x):
+                for y in range(1,size_y):
+                    if s1[x-1]==s2[y-1]:
+                        matrix[x,y]=min(matrix[x-1,y]+1,matrix[x-1,y-1],matrix[x,y-1]+1) 
+                    else:
+                        matrix[x,y]=min(matrix[x-1,y]+1,matrix[x-1,y-1]+1,matrix[x,y-1]+1)
+            print(matrix) 
+            return(matrix[size_x-1,size_y-1])
+s1="Anuja" 
+s2="Anoja"
+print( levenshelin(s1,s2))
